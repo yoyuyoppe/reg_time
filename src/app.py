db763bda5087b6425d520a4923c78409d91f0aaa
Артем О. Боровик <borovikao@shkurenko.local>
2017-02-21 20:14:09 +0600

    Обновление src от 2017-02-21

--- src/app.py
+++ src/app.py
@@ -1,21 +1,25 @@
 from flask import Flask, render_template, request, make_response
+from database import DB
+from datetime import datetime
 
 app = Flask(__name__)
+local_db = DB()
 
 
 def valid_login(login, passw):
-    result = True
-    if login == "" or passw == "":
-        result = False     
+    """Проверяет, зарегистрирован пользователь или нет"""
+    sql = 'select * from users where name = ? and password = ?'
+    return False if local_db.__execute__(sql, [login, passw]) == [] else True
 
-    return result    
-    
+def fix_visit(time_visit):
+    """Фиксирует начало визита пользователя и последний его уход в течение дня"""
+    pass
 
 @app.route('/')
 def index():
     _username = "" if not request.cookies.get('username') else request.cookies.get('username')
     _passw = "" if not request.cookies.get('passw') else request.cookies.get('passw')
-    #return 'login: %s, password: %s' % (_username, _passw)
+
     return render_template('index.html', username=_username, passw=_passw)
 
 
@@ -25,8 +29,10 @@ def login():
     info = None
     _username = request.form['login']
     _passw = request.form['password']
+    curr_time = datetime.strftime(datetime.now(), "%Y.%m.%d %H:%M:%S")
     if valid_login(_username, _passw):
-        info = 'Авторизация прошла успешно. login: %s, password: %s' % (_username, _passw)
+        info = 'Авторизация прошла успешно '+ curr_time
+        fix_visit(curr_time)
     else:
         info = 'Неверный логин или пароль'
 
@@ -38,3 +44,4 @@ def login():
 
 if __name__ == '__main__':
     app.run()
+    
--- src/database.py
+++ src/database.py
@@ -3,23 +3,35 @@ import sqlite3, os
 class DB():
     """Класс для работы с базой данных sqllite"""
 
-    CONNECTION_STRING = 'reg_time.db'    
+    CONNECTION_STRING = 'reg_time.db'
+    CONNECTOR = None    
 
-    def __init__(self, connector):
-        """Подключает локальную базу данных. Если база не найдена, выдает ошибку"""       
-        if os.path.exists(self.CONNECTION_STRING):
+    def __init__(self):
+        """Подключает локальную базу данных. Если база не найдена, выдает ошибку""" 
+        if not os.path.exists(self.CONNECTION_STRING):
             raise 'База данных не найдена'   
 
-        try:
-            self.connector = sqlite3.connect(self.CONNECTION_STRING)
-        except Exception(e):
-            raise 'Попытка подключения к базе данных завершилась неудачно по причине: '+str(e)
+        self.CONNECTOR = sqlite3.connect(self.CONNECTION_STRING)
 
     def __execute__(self, command, param = []):
         """Выполняет переданную команду sql с параметрами или без"""
-        c = self.connector.cursor()
+        c = self.CONNECTOR.cursor()
         result = c.execute(command, param)
         return result
 
     def __del__(self):
-        self.connector.close()
+        self.CONNECTOR.close()
+        print('finally class DB')
+
+"""try:
+    _db = DB()
+except Exception(e):
+    print('ERROR: %s', e)    
+
+for row in (_db.__execute__('select id, name from users where name = ? and password = ?', ["Администратор", "pkjqjlvby"])):
+    print(row[0])
+
+c = _db.__execute__('select * from users where name = ? and password = ?', ["Администратор", "pkjqjlvby"])
+print(c.fetchall())
+
+_db = None"""    
