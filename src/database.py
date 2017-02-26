5c99938407eba223c79752ee51433375a188ac22
Артем О. Боровик <borovikao@shkurenko.local>
2017-02-26 21:38:57 +0600

    update from 20170226

--- src/database.py
+++ src/database.py
@@ -3,35 +3,34 @@ import sqlite3, os
 class DB():
     """Класс для работы с базой данных sqllite"""
 
-    CONNECTION_STRING = 'reg_time.db'
-    CONNECTOR = None    
+    CONNECTION_STRING = 'reg_time.db'    
 
     def __init__(self):
         """Подключает локальную базу данных. Если база не найдена, выдает ошибку""" 
-        if not os.path.exists(self.CONNECTION_STRING):
-            raise 'База данных не найдена'   
+        if not os.path.exists(DB.CONNECTION_STRING):
+            raise 'File database not found'   
 
-        self.CONNECTOR = sqlite3.connect(self.CONNECTION_STRING)
+        self.connector = sqlite3.connect(DB.CONNECTION_STRING)
 
     def __execute__(self, command, param = []):
-        """Выполняет переданную команду sql с параметрами или без"""
-        c = self.CONNECTOR.cursor()
+        """
+        Выполняет переданную команду sql с параметрами или без
+        В результате возвращает список найденных строк
+        """
+        c = self.connector.cursor()
         result = c.execute(command, param)
-        return result
+        return result.fetchall()
 
     def __del__(self):
-        self.CONNECTOR.close()
-        print('finally class DB')
+        self.connector.close()
 
-"""try:
-    _db = DB()
-except Exception(e):
-    print('ERROR: %s', e)    
+
+"""_db = DB()
 
 for row in (_db.__execute__('select id, name from users where name = ? and password = ?', ["Администратор", "pkjqjlvby"])):
     print(row[0])
 
 c = _db.__execute__('select * from users where name = ? and password = ?', ["Администратор", "pkjqjlvby"])
-print(c.fetchall())
+print(c[0][0])
 
-_db = None"""    
+_db = None"""  
